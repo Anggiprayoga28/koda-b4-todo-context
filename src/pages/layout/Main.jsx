0dda93b830ef8f58ee111e4008ee26b707ff4d3d
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-10-09 09:09:06 +0700

    Feat: Refactor Main component to manage todos by date and integrate DateContext

--- src/pages/layout/Main.jsx
+++ src/pages/layout/Main.jsx
@@ -5,31 +5,47 @@ import {TodoModalContext} from '../../components/ModalContext'
 import Modal from '../../components/Modal'
 import TodoContext from '../../components/TodoContext'
 import AddTodoForm from '../../components/AddTodoForm'
+import DateContext from '../../components/DateContext'
 
 function Main() {
   const [showModal, setShowModal] = React.useState(false)
+  const [selectedDate, setSelectedDate] = React.useState(() => {
+    const today = new Date()
+    return today.toISOString().split('T')[0] 
+  })
   
-  const [todo, setTodo] = React.useState(() => {
-    const savedTodos = localStorage.getItem('todos')
-    return savedTodos ? JSON.parse(savedTodos) : []
+  const [allTodos, setAllTodos] = React.useState(() => {
+    const savedTodos = localStorage.getItem('allTodos')
+    return savedTodos ? JSON.parse(savedTodos) : {}
   })
 
   React.useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todo))
-  }, [todo])
+    localStorage.setItem('allTodos', JSON.stringify(allTodos))
+  }, [allTodos])
+
+  const currentDateTodos = allTodos[selectedDate] || []
+
+  const setCurrentDateTodos = (newTodos) => {
+    setAllTodos({
+      ...allTodos,
+      [selectedDate]: newTodos
+    })
+  }
 
   return (
-    <TodoContext.Provider value={{data:todo, setData: setTodo}}>
+    <TodoContext.Provider value={{data:currentDateTodos, setData: setCurrentDateTodos}}>
       <TodoModalContext.Provider value={{showModal, setShowModal}}>
-        <div className='bg-gray-200 min-h-screen relative'>
-          <div className='max-w-md w-full mx-auto relative'>
-            <div className='p-2 bg-gray-100 min-h-[calc(theme(height.screen)-theme(height.12))] flex flex-col'>
-              <Outlet />
+        <DateContext.Provider value={{selectedDate, setSelectedDate}}>
+          <div className='bg-gray-200 min-h-screen relative'>
+            <div className='max-w-md w-full mx-auto relative'>
+              <div className='p-2 bg-gray-100 min-h-[calc(theme(height.screen)-theme(height.12))] flex flex-col'>
+                <Outlet />
+              </div>
+              <Navbar />
+              {showModal && <AddTodoModal />}
             </div>
-            <Navbar />
-            {showModal && <AddTodoModal />}
           </div>
-        </div>
+        </DateContext.Provider>
       </TodoModalContext.Provider>
     </TodoContext.Provider>
   )
